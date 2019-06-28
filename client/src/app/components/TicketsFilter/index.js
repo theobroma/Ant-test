352587cfc65dc9072831bce29a9002db6cc8ed48
Alexandr Siryj <theobroma222@gmail.com>
2019-06-29 01:01:54 +0300

    add checkbox

--- client/src/app/components/TicketsFilter/index.js
+++ client/src/app/components/TicketsFilter/index.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Radio, Icon } from 'antd';
+import { Button, Radio, Checkbox, Icon } from 'antd';
 import StyledTicketsFilter from './TicketsFilter.styled';
 
-// const TicketsFilter = (props) => {
-//   console.log(props);
-//   return (
-//     <StyledTicketsFilter>
-//       <div className="pa3">
-//         <Radio.Group value={size} onChange={this.handleSizeChange}>
-//           <Radio.Button value="large">Large</Radio.Button>
-//           <Radio.Button value="default">Default</Radio.Button>
-//           <Radio.Button value="small">Small</Radio.Button>
-//         </Radio.Group>
-//       </div>
-//     </StyledTicketsFilter>
-//   );
-// };
+const CheckboxGroup = Checkbox.Group;
+
+const plainOptions = ['Без пересадок', '1 пересадка', '2 пересадки', '3 пересадки'];
+const defaultCheckedList = ['Без пересадок'];
 
 class TicketsFilter extends React.Component {
   state = {
+    // radio
     currency: 'UAH',
+    // checkbox
+    checkedList: defaultCheckedList,
+    indeterminate: true,
+    checkAll: false,
   };
 
   handleCurrencyChange = (e) => {
     this.setState({ currency: e.target.value });
+  };
+
+  onChange = (checkedList) => {
+    this.setState({
+      checkedList,
+      indeterminate: !!checkedList.length && checkedList.length < plainOptions.length,
+      checkAll: checkedList.length === plainOptions.length,
+    });
+  };
+
+  onCheckAllChange = (e) => {
+    this.setState({
+      checkedList: e.target.checked ? plainOptions : [],
+      indeterminate: false,
+      checkAll: e.target.checked,
+    });
   };
 
   render() {
@@ -39,6 +50,23 @@ class TicketsFilter extends React.Component {
               <Radio.Button value="USD">USD</Radio.Button>
               <Radio.Button value="EUR">EUR</Radio.Button>
             </Radio.Group>
+          </div>
+          {/* checkbox */}
+          <div className="form-group">
+            <label class="control-label">Количество пересадок</label>
+            <Checkbox
+              indeterminate={this.state.indeterminate}
+              onChange={this.onCheckAllChange}
+              checked={this.state.checkAll}
+            >
+              Все
+            </Checkbox>
+            <CheckboxGroup
+              options={plainOptions}
+              value={this.state.checkedList}
+              onChange={this.onChange}
+              className="flex flex-column"
+            />
           </div>
         </div>
       </StyledTicketsFilter>
