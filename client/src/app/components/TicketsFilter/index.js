c3a206eb8d0102ea73656a71e58455d5701d0b50
Alexandr Siryj <theobroma222@gmail.com>
2019-06-28 23:35:06 +0300

    radio buttons

--- client/src/app/components/TicketsFilter/index.js
+++ client/src/app/components/TicketsFilter/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Radio, Icon } from 'antd';
 import StyledTicketsFilter from './TicketsFilter.styled';
 
-const TicketsFilter = (props) => {
-  console.log(props);
-  return (
-    <StyledTicketsFilter>
-      <div className="pa3">StyledTicketsFilter</div>
-    </StyledTicketsFilter>
-  );
-};
+// const TicketsFilter = (props) => {
+//   console.log(props);
+//   return (
+//     <StyledTicketsFilter>
+//       <div className="pa3">
+//         <Radio.Group value={size} onChange={this.handleSizeChange}>
+//           <Radio.Button value="large">Large</Radio.Button>
+//           <Radio.Button value="default">Default</Radio.Button>
+//           <Radio.Button value="small">Small</Radio.Button>
+//         </Radio.Group>
+//       </div>
+//     </StyledTicketsFilter>
+//   );
+// };
+
+class TicketsFilter extends React.Component {
+  state = {
+    currency: 'UAH',
+  };
+
+  handleCurrencyChange = (e) => {
+    this.setState({ currency: e.target.value });
+  };
+
+  render() {
+    const { currency } = this.state;
+    return (
+      <StyledTicketsFilter>
+        <div className="pa3">
+          <div className="form-group">
+            <label class="control-label">Валюта</label>
+            <Radio.Group value={currency} onChange={this.handleCurrencyChange}>
+              <Radio.Button value="UAH">UAH</Radio.Button>
+              <Radio.Button value="USD">USD</Radio.Button>
+              <Radio.Button value="EUR">EUR</Radio.Button>
+            </Radio.Group>
+          </div>
+        </div>
+      </StyledTicketsFilter>
+    );
+  }
+}
 
 TicketsFilter.propTypes = {};
 
