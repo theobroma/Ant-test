f66471187a460a4e8da8776cdfee8ea1360ad5fd
Alexandr Siryj <theobroma222@gmail.com>
2019-07-11 23:32:57 +0300

    UglifyJsPlugin

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,15 +1,26 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common.js');
 
 const config = {};
 
 config.devtool = 'none';
-//turn off minimize and UglifyJSPlugin to see pretty output bundle
+// turn off minimize and to see pretty output bundle
 config.optimization = {
   minimize: true,
+  minimizer: [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+      },
+    }),
+  ],
 };
 
 config.plugins = [
@@ -18,25 +29,6 @@ config.plugins = [
       NODE_ENV: JSON.stringify('production'),
     },
   }),
-  // new UglifyJSPlugin({
-  //   cache: true,
-  //   parallel: true,
-  //   uglifyOptions: {
-  //     output: {
-  //       comments: false,
-  //       beautify: false
-  //     },
-  //     compress: {
-  //       sequences: true,
-  //       booleans: true,
-  //       loops: true,
-  //       unused: true,
-  //       warnings: false,
-  //       drop_console: true,
-  //       unsafe: true
-  //     }
-  //   }
-  // }),
   new OptimizeCSSAssets(),
 ];
 
