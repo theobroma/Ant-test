9727c50314493e810ed27db154df83cbd826c4aa
Alexandr Siryj <theobroma222@gmail.com>
2019-07-17 23:42:37 +0300

    upd autoprefixer

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,14 +1,15 @@
+/* eslint global-require: 0 */
 module.exports = {
   plugins: [
-    require('autoprefixer')({ browsers: ['last 2 versions'] }),
+    require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('css-mqpacker')({
-      sort: true
+      sort: true,
     }),
     require('postcss-custom-media'),
     require('postcss-media-minmax'),
     require('postcss-fontpath')({
-      checkFiles: true
-    })
-  ]
+      checkFiles: true,
+    }),
+  ],
 };
